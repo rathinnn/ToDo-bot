cda42743465336d76812551ee2380b6f8b887fec
rathinnn <56340004+rathinnn@users.noreply.github.com>
2020-08-08 09:43:14 +0530

    using commands
    using bot commands instead of client

--- trial.py
+++ trial.py
@@ -1,16 +1,61 @@
+
+
+
+
+  
 import discord
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+from discord.ext import commands
 
-client = MyClient()
+f = open("test.txt",'r')
+data=f.read().splitlines()
+
+data = list(filter(None, data))
+f.close()
+
+client = commands.Bot(command_prefix="<#>")
+
+@client.command()
+async def hello(ctx, arg):
+    
+    
+    await ctx.send('Hello {}'.format(ctx.author))
+
+@client.command()
+async def Add(ctx, arg1, arg2):
+
+    
+    data.append(str(arg2))
+    
+    await ctx.send('Adding {} to List Of Assignments'.format(arg2))
+
+@client.command()
+async def display(ctx):
+
+    for dt in data:
+        await ctx.send('Assignments Pending: {} '.format(dt))
+        
+    
+    
+    
+
+
+    
+
+
+
+
+
+
 client.run('NzQxMzIwNjIwNzA4NDYyNjkz.Xy12oQ.DfVvVvZjBhy-5Lkz48tHXUKVqxU')
+
+f = open("test.txt",'w')
+
+for i in range (len(data)):
+    if i is not len(data)-1:
+        data[i]=data[i]+'\n'
+    
+f.writelines(data)
+f.close()
