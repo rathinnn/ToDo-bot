7890ec1056c3c8357362ef46ca06a3eb1c4e9529
rathinnn <56340004+rathinnn@users.noreply.github.com>
2020-08-08 17:14:20 +0530

    changed request

--- trial.py
+++ trial.py
@@ -7,7 +7,7 @@ import discord
 
 
 
-from discord.ext import commands
+from discord.ext import commands,tasks
 
 
 
@@ -18,8 +18,21 @@ client = commands.Bot(command_prefix=".")
 @client.command()
 async def hello(ctx):
     
+    author=ctx.author
     
-    await ctx.send('Hello {}'.format(ctx.author))
+    if(str(author)=='Blaze777#2388'):
+        await ctx.send('Hello Dear Boss ')
+
+
+
+    elif(str(author)=='siddharthc30#7363'):
+        await ctx.send('Hey Hows Rowdy ')
+
+    
+        
+    
+    else:
+        await ctx.send('Hello {}'.format(ctx.author))
 
 @client.command()
 async def Add(ctx, arg1, arg2):
@@ -29,39 +42,32 @@ async def Add(ctx, arg1, arg2):
     
     tmessage=await ctx.send('Assignment {}'.format(arg2))
     await tmessage.pin()
-    
-    
-
-
-@client.event
-async def on_reaction_add(reaction, user):
-    cemoji=reaction.emoji
-    if(cemoji == "👌"):
-        tmessage=reaction.message
-        if tmessage.author == client.user:
-            await tmessage.channel.send('pinning this message :wink:')
-            await tmessage.pin()
-            
-
-   
-     
-            
-
-    else:
-        tmessage=reaction.message
-        await tmessage.channel.send('I wont do anything k? :unamused:')
-        
-        
-    
-    
-    
 
 
 @client.command()
 async def display(ctx):
 
-    for dt in data:
-        await ctx.send('Assignments Pending: {} '.format(dt))
+    list=await ctx.pins()
+    print(list)
+
+    for li in list:
+        await ctx.send(li.content)
+        
+    
+
+    
+    
+
+
+
+        
+        
+    
+    
+    
+
+
+
         
     
     
