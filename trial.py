93366dc486b23b9c95f37f088ed27dfdd51bdcf5
rathinnn <56340004+rathinnn@users.noreply.github.com>
2020-08-09 18:00:23 +0530

    added modify

--- trial.py
+++ trial.py
@@ -1,7 +1,5 @@
 
 
-
-
   
 import discord
 
@@ -9,9 +7,27 @@ import discord
 
 from discord.ext import commands,tasks
 
+from datetime import datetime
+
+import asyncio
 
 
-client = commands.Bot(command_prefix=".")
+tempcode='''from datetime import datetime
+message = "biology;assignment 1;21-12-2021;18:14:33"
+message.replace(" ", "")
+message_array = message.split(";")
+subject_of_assignment = message_array[0].strip();
+assignment_name = message_array[1].strip()
+date_time_string = message_array[2].strip() + " " + message_array[3].strip()
+date_time = datetime.strptime(date_time_string, "%d-%m-%Y %H:%M:%S")
+print("subject:" + subject_of_assignment)
+print("Assignment_name:" + assignment_name)
+print(date_time)
+biology;assignment 1;21-12-2021;18:14:33
+biology;assignment 1;21-12-2021;18:14:33
+biology ;assignment 1;21-12-2021;18:14:33'''
+
+client = commands.Bot(command_prefix="!!")
 
     
 
@@ -35,50 +51,199 @@ async def hello(ctx):
         await ctx.send('Hello {}'.format(ctx.author))
 
 @client.command()
-async def Add(ctx, arg1, arg2):
+async def Add(ctx, arg1):
 
+    message=arg1
+    message.replace(" ", "")
+    message_array = message.split(";")
+    if(len(message_array) != 4):
+        await ctx.send('Invalid Format')
+        return
+        
+    subject_of_assignment = message_array[0].strip()
+    assignment_name = message_array[1].strip()
+    date_time_string = message_array[2].strip() + " " + message_array[3].strip()
+
+    try:
+        
+        date_time = datetime.strptime(date_time_string, "%d-%m-%Y %H:%M:%S")
+        tmessage=await ctx.send('{}   {}   {}'.format(subject_of_assignment,assignment_name,date_time_string))
+        await tmessage.pin()
+    except:
+        await ctx.send('Invalid Format ')
     
     
     
-    tmessage=await ctx.send('Assignment {}'.format(arg2))
-    await tmessage.pin()
 
 
 @client.command()
 async def display(ctx):
 
     list=await ctx.pins()
-    print(list)
+    
 
     for li in list:
         await ctx.send(li.content)
+
+
+@client.command()
+async def checktime(ctx):
+    list2=await ctx.pins()
+    await ctx.send('Assignments with Deadlines approaching')
+    for li in list2:
+        
+        message=li.content
+        message_array = message.split()
+        subject_of_assignment = message_array[0].strip();
+        assignment_name = message_array[1].strip()
+        date_time_string = message_array[2].strip() + " " + message_array[3].strip()
+        date_time = datetime.strptime(date_time_string, "%d-%m-%Y %H:%M:%S")
+        
+        now=datetime.now()
+        difference=date_time-now
+        difference=difference.days
+        
+        
+            
+            
+        if(date_time<=now):
+            
+            await li.unpin()
+        
+        
+        elif(difference<=2 ):
+            
+            await ctx.send(li.content+' :  in '+str(difference)+'  days')
+
+        
+@client.command()
+async def delete(ctx):
+
+    i=1
+    cmessage=ctx.message
+    
+    list=await ctx.pins()
+    await ctx.send('Choose Assingment to Delete')
+    for li in list:
+        await ctx.send(str(i)+': '+li.content)
+
+        i=i+1
+
+    def check2(m):
+        return m.author == cmessage.author
+
+   
+    
+    
+
+    try:
+        msg = await client.wait_for('message', check=check2, timeout=30.0)
+    except asyncio.TimeoutError:
+        return await ctx.send('Sorry, you took too long')
+
+    try:
+        todel=msg.content
+        todel=int(todel)
+        li=list[todel-1]
+        await li.unpin()
+        await ctx.send('deleted '+li.content)
+
+    except:
+        await ctx.send('Something went Wrong')
+        
         
     
+    
 
+    
+
+
+
+@client.command()
+async def modify(ctx):
+
+    i=1
+    cmessage=ctx.message
+    
+    list=await ctx.pins()
+    await ctx.send('Choose Assingment to Modify')
+    for li in list:
+        await ctx.send(str(i)+': '+li.content)
+
+        i=i+1
+
+    def check3(m):
+        return m.author == cmessage.author
+
+   
     
     
 
+    try:
+        msg = await client.wait_for('message', check=check3, timeout=30.0)
+    except asyncio.TimeoutError:
+        return await ctx.send('Sorry, you took too long')
+
+    try:
+        todel=msg.content
+        todel=int(todel)
+        li=list[todel-1]
+        
+
+    except:
+        return await ctx.send('Something went Wrong')
+        
+    
+    toedit=li.content
+    message_array = toedit.split()
+    subject_of_assignment = message_array[0].strip();
+    assignment_name = message_array[1].strip()
+    await ctx.send('Enter New Time')
+
+    try:
+        msg = await client.wait_for('message', check=check3, timeout=30.0)
+    except asyncio.TimeoutError:
+        return await ctx.send('Sorry, you took too long')
+    
+    
+    toput=msg.content
+    message_array = toput.split(";")
+    try:
+        date_time_string = message_array[0].strip() + " " + message_array[1].strip()
+        date_time = datetime.strptime(date_time_string, "%d-%m-%Y %H:%M:%S")
+        tmessage=await ctx.send('{}   {}   {}'.format(subject_of_assignment,assignment_name,date_time_string))
+        await tmessage.pin()
+    except:
+        return await ctx.send('Invalid Format ')
+        
+        
+
+   
+    
+  
+    
+    
+        
+
+        
 
 
+    
+        
+
+            
+
+        
+            
+        
+        
+        
+
+    
         
         
     
     
-    
-
-
-
-        
-    
-    
-    
-
-
-    
-
-
-
-
 
 
 client.run('NzQxMzIwNjIwNzA4NDYyNjkz.Xy12oQ.DfVvVvZjBhy-5Lkz48tHXUKVqxU')
